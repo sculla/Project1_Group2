52cae11dc8cf249bb8197c58db42df8844f79bc0
sculla <sculla@u.washington.edu>
2019-07-03 00:09:04 -0700

    #working on the turnstile data
    merged the notebook, exploring. see slack for the graph which showed a few weird ones

--- ETL.py
+++ ETL.py
@@ -1,22 +1,10 @@
 
-PATH = 'turnstile_190629.txt'
+# Fatima
+# TODO StationNames
 
-# RangeIndex: 205963 entries, 0 to 205962
-# Data columns (total 11 columns):
-# C/A                                                                     205963 non-null object
-# UNIT                                                                    205963 non-null object
-# SCP                                                                     205963 non-null object
-# STATION                                                                 205963 non-null object
-# LINENAME                                                                205963 non-null object
-# DIVISION                                                                205963 non-null object
-# DATE                                                                    205963 non-null object
-# TIME                                                                    205963 non-null object
-# DESC                                                                    205963 non-null object
-# ENTRIES                                                                 205963 non-null int64
-# EXITS                                                                   205963 non-null int64
-# dtypes: int64(2), object(9)
-# memory usage: 17.3+ MB
+# aaron's td
 
+# TODO enries per time exits per time
 
 
 def extract(filename= 'turnstile_190629.txt'):
@@ -26,28 +14,39 @@ def extract(filename= 'turnstile_190629.txt'):
     :return: Pandas DataFrame of the MTA week's data with a datetime object in place of the date & time objects
     """
 
-    from datetime import datetime
-
-    #Fatima
-    #TODO StationNames
-
-
-    #aaron's td
-
-    #TODO enries per time exits per time
 
     import pandas as pd
     df = pd.read_csv(filename)
-    dt_format = '%m/%d/%Y %H:%M:%S'
-    working = []
-    for _, row in enumerate(df.values):
-        working.append(datetime.strptime(row[6]+' '+row[7], dt_format))
-    df['datetime'] = pd.DataFrame(working)
-    df.drop(columns=['DATE','TIME'], inplace=True)
+
     return df
 
 
 
+def transform(df):
+    import pandas as pd
+    from datetime import datetime
+
+    #DateTime transform
+    dt_format = '%m/%d/%Y %H:%M:%S'
+    working = []
+
+    for _, row in enumerate(df.values):
+        working.append(datetime.strptime(row[6]+' '+row[7], dt_format))
+    df['datetime'] = pd.DataFrame(working)
+    df.drop(columns=['DATE', 'TIME'], inplace=True)
+    df.to_pickle('.DateTime_transformed.pickle')
+
+    #column names
+    df.columns = df.columns.str.strip()
+
+    #delta turnstiles
+    cols_diff = ["ENTRIES", "EXITS"]
+    cols_add = ["TURNSTILE_ENTRIES", "TURNSTILE_EXITS"]
+    df[cols_add] = df.groupby(by=["STATION", "C/A", "SCP", "LINENAME"])[cols_diff].diff()
+    df.to_pickle('.Turnstile_transformed.pickle')
+
+    return df
+
+
 if __name__ == "__main__":
-    df = extract()
-    print(df.columns)
+    transform(extract())
--- README.md
+++ README.md
@@ -1,26 +1,2 @@
-# Project1_Group2 -- Metis MTA Project
-
-## Making a Feature Branch for your contribution:
-When making a branch for your part of the project, please use the feature/<your branch name> to denote your part of the project. After the successful completion of the feature, we will merge it to the develop branch.  
-With this, we are able to easily each work off of a single branch, develop, and have our features able to be pushed back to that branch for the full integration. 
-
-Please let me know if you're having any issues with this, I've included a link here to review as well [Git Flow](https://www.atlassian.com/git/tutorials/comparing-workflows/gitflow-workflow) 
-
---@sculla
-
-## MVP - Stations that will get money/productive canviser  
-"Send your staff with these daytime hours of 6am - 8pm to these 10 stations for the best turn out"  
-* busiest stations  
-* sustained flow  
-
-### Pivot List: Sub Regions for Tech centers / Universities  
-* diversity of buroughs  
-* tech hubs?  
-## Please check issue list
-
-Feature ideas:
-* adding a geo heat map  
-** pulling second DB that maps the names of the stations to an address  
-* turnstile graphs for the traffic by hour?  
-** not just the most popular 
-* remove tourism hubs  
+# Project1_Group2
+Metis MTA 
